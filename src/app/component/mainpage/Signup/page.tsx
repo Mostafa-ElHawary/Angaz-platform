60245e0d7be082e5456c8af45b31097ecf37afda
Mostafa-ElHawary <alhwarymostafa@gmail.com>
2023-12-05 16:57:42 +0200

    editing tsx syntax of Signup & Login

--- src/app/component/mainpage/Signup/page.tsx
+++ src/app/component/mainpage/Signup/page.tsx
@@ -4,36 +4,36 @@ import Image from "next/image";
 import Link from "next/link";
 import Support from "@/app/assets/svgImg/undraw_showing_support_re_5f2v.svg";
 
-export default function Signup() {
+const Signup: React.FC = () => {
   return (
     <section className="flex flex-col items-center justify-center h-screen">
-      <div className="p-4 md:p-8 rounded-md w-full max-w-md">
+      <div className="p-4 md:p-8 rounded-md w-full max-w-md ">
         <div className="text-center mb-6">
           <Image src={Support} alt="Your Image Alt" width={600} height={250} />
         </div>
 
         <form className="flex flex-col">
-          <h3 className="text-2xl font-bold mt-4">Create a New Account</h3>
-          <p className="text-sm text-gray-500 mb-4">Write your signup data</p>
+          <h3 className="text-3xl font-bold text-white mt-4">Create a New Account</h3>
+          <p className="text-sm text-white mb-4">Write your signup data</p>
 
-          <button className="bg-blue-500 text-white py-2 rounded-md mb-4 hover:bg-blue-600 transition w-full">
+          <button className="bg-yellow-400 text-gray-800 py-2 rounded-md mb-4 hover:bg-yellow-500 transition w-full">
             Sign up with Google
           </button>
 
-          <div className="mb-4 text-center">or</div>
+          <div className="mb-4 text-center text-white">or</div>
 
           {[
             { label: "First Name", type: "text" },
             { label: "Last Name", type: "text" },
             { label: "Your Email", type: "email" },
-            { label: "Your Number", type: "number" },
+            { label: "Your Number", type: "tel" },
             { label: "Your Password", type: "password" },
             { label: "Confirm Password", type: "password" },
           ].map((input, index) => (
             <Input key={index} {...input} />
           ))}
 
-          <p className="text-sm mt-4 mb-6 text-gray-500">
+          <p className="text-sm mt-4 mb-6 text-white">
             By registering in Enjaz, you agree to the privacy, terms, and conditions
           </p>
 
@@ -44,9 +44,9 @@ export default function Signup() {
             Sign Up
           </button>
 
-          <p className="text-sm text-center">
+          <p className="text-sm text-center text-white">
             Already have an account?{' '}
-            <Link href={``} className="text-blue-500 hover:underline">
+            <Link href={``} className="text-blue-200 hover:underline">
               Log in
             </Link>
           </p>
@@ -54,20 +54,27 @@ export default function Signup() {
       </div>
     </section>
   );
+};
+
+interface InputProps {
+  label: string;
+  type: string;
 }
 
-function Input({ label, type, placeholder }: any) {
+const Input: React.FC<InputProps> = ({ label, type }) => {
   return (
     <div className="mb-4">
-      <label htmlFor={label} className="block text-sm font-semibold mb-1">
+      <label htmlFor={label} className="block text-sm font-semibold text-white mb-1">
         {label}
       </label>
       <input
         type={type}
         id={label.toLowerCase().replace(' ', '-')}
-        className="input-field"
-        placeholder={placeholder}
+        className="input-field bg-gray-100 text-gray-800 placeholder-gray-500 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+        placeholder={`Enter ${label}`}
       />
     </div>
   );
-}
+};
+
+export default Signup;
--- src/app/component/mainpage/login/page.tsx
+++ src/app/component/mainpage/login/page.tsx
@@ -1,71 +1,98 @@
 import Image from "next/image";
 import Link from "next/link";
-import coffe from "@/app/assets/svgImg/undraw_coffee_time_e8cw.svg"
-export default function Login() {
+import coffe from "@/app/assets/svgImg/undraw_coffee_time_e8cw.svg";
+
+
+interface LoginProps {
+  backgroundImage: string;
+}
+
+const Login: React.FC<LoginProps> = ({ backgroundImage }) => {
   return (
-    <section className="flex flex-col items-center justify-center h-screen">
-      <div className="p-8 rounded-md shadow-md w-full max-w-md ">
-        <div className="text-center mb-8">
-          <Image src={coffe} alt="Your Image Alt" width={500} height={80} />
-        </div>
-
-        <form className="flex flex-col">
-          <h3 className="text-3xl font-extrabold mt-4 text-gray-800">
+    <section className="flex flex-col items-center justify-center h-screen bg-gray-900">
+      <Image src={coffe} alt="Your Image Alt" width={500} height={80} />
+      <div className="container mx-auto mt-8 p-8 bg-white rounded-xl shadow-2xl flex flex-col md:flex-row">
+        <div className="flex-1 md:p-8">
+          <h2 className="text-4xl font-extrabold text-indigo-600 mb-4">
             Welcome Back!
-          </h3>
-          <p className="text-sm text-gray-500">Log in to your account</p>
-          <button className="btn-google mb-4">Sign in with Google</button>
-
-          <div className="divider mb-4"></div>
-
-          {[
-            { label: "Your Email", type: "email" },
-            { label: "Your Password", type: "password" },
-          ].map((input, index) => (
-            <Input key={index} {...input} />
-          ))}
-
-          <div className="flex items-center justify-between mb-4">
-            <div className="flex items-center">
-              <input type="checkbox" id="remember" className="mr-2" />
-              <label htmlFor="remember" className="text-sm text-gray-600">
-                Remember me
-              </label>
-            </div>
-            <Link href={``} className="text-sm text-blue-500 hover:underline">
-              Forgot Password?
+          </h2>
+          <p className="text-lg text-gray-700 mb-6">Log in to your account</p>
+          <div className="mb-6 space-y-4">
+            <Link
+              href={``}
+              passHref
+              className="btn-google flex items-center justify-center bg-red-500 text-white py-3 rounded-lg hover:bg-red-600 transition w-full"
+            >
+              <i className="fab fa-google mr-2"></i> Sign in with Google
             </Link>
           </div>
-
-          <button className="btn-login mb-4">Log In</button>
-
-          <p className="text-sm text-gray-600 text-center">
+          <div className="border-b border-gray-400 mb-6"></div>
+          <form className="flex flex-col space-y-4">
+            <Input label="Your Email" type="email" />
+            <Input label="Your Password" type="password" />
+            <div className="flex items-center justify-between mb-4">
+              <div className="flex items-center">
+                <input type="checkbox" id="remember" className="mr-2" />
+                <label htmlFor="remember" className="text-sm text-gray-600">
+                  Remember me
+                </label>
+              </div>
+              <Link
+                href={``}
+                passHref
+                className="text-sm text-indigo-500 hover:underline"
+              >
+                Forgot Password?
+              </Link>
+            </div>
+            <button
+              type="button"
+              className="bg-indigo-600 text-white py-3 rounded-lg hover:bg-indigo-700 transition focus:outline-none w-full"
+            >
+              Log In
+            </button>
+          </form>
+          <p className="text-lg text-gray-700 mt-6">
             Don't have an account?{' '}
-            <Link href={``} className="text-blue-500 hover:underline">
+            <Link
+              href={``}
+              passHref
+              className="text-indigo-500 hover:underline"
+            >
               Sign Up
             </Link>
           </p>
-        </form>
+        </div>
+        <div className="hidden md:flex md:items-center md:justify-center md:p-8">
+          <Image src={backgroundImage} alt="" />
+        </div>
       </div>
     </section>
   );
+};
+
+interface InputProps {
+  label: string;
+  type: string;
 }
 
-function Input({ label, type }: any) {
+const Input: React.FC<InputProps> = ({ label, type }) => {
   return (
-    <div className="mb-4">
+    <div className="mb-6">
       <label
         htmlFor={label}
-        className="block text-sm font-semibold text-gray-600 mb-1"
+        className="block text-lg font-semibold text-gray-700 mb-1"
       >
         {label}
       </label>
       <input
         type={type}
-        id={label.toLowerCase().replace(" ", "-")}
-        className="input-field focus:ring focus:ring-blue-500"
+        id={label.toLowerCase().replace(' ', '-')}
+        className="input-field px-4 py-2 rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500"
         placeholder={label}
       />
     </div>
   );
-}
+};
+
+export default Login;
